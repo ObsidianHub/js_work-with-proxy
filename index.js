ac0cca35e4b9a428a0f55bde8c0845f4493a1fa2
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-01-25 23:55:54 +0200

    add Proxy optimization method to array

--- index.js
+++ index.js
@@ -21,3 +21,40 @@ const withHiddenProps = (target, prefix = "_") => {
     get: (obj, prop, receiver) => (prop in receiver ? obj[prop] : void 0)
   });
 };
+
+const data = withHiddenProps({
+  name: "Denny",
+  age: 26,
+  _uid: "123123"
+});
+
+// Optimization
+const IndexedArray = new Proxy(Array, {
+  construct(target, [args]) {
+    const index = {};
+    args.forEach(item => (index[item.id] = item));
+
+    return new Proxy(new target(...args), {
+      get(arr, prop) {
+        switch (prop) {
+          case "push":
+            return item => {
+              index[item.id] = item;
+              arr[prop].call(arr, item);
+            };
+          case "fingById":
+            return id => index[id];
+          default:
+            return arr[prop];
+        }
+      }
+    });
+  }
+});
+
+const users = new IndexedArray([
+  { id: 11, name: "Denny", job: "Fullstack", age: 26 },
+  { id: 22, name: "Katya", job: "Artist", age: 23 },
+  { id: 33, name: "Nazar", job: "Lawyer", age: 26 },
+  { id: 44, name: "Misha", job: "Programmer", age: 26 }
+]);
